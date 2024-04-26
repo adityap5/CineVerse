86794d8bf7d3566befa58d0fd6fa95e7512516bb
adityap5 <aditya29pippal@gmail.com>
2024-04-26 23:48:37 +0530

    moviehub

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import './style.scss'
+
+import useFetch from '../../../hooks/useFetch'
+import Img from '../../../components/lazyLoadImage/Img'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+const HomeBanner = () => {
+  const [background, setBackground] = useState("")
+  const [query, setQuery] = useState("")
+  const navigate = useNavigate()
+  const { url } = useSelector((state) => state.home)
+  const { data, loading } = useFetch("/movie/upcoming")
+
+  useEffect(() => {
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg)
+  }, [data])
+
+  const searchQueryHandler = (e) => {
+    
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  return (
+    <div className="herobanner">
+      {!loading &&
+        <div className="backdrop-img">
+          <Img src={background} />
+
+        </div>
+      }
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="Subtitle"> millions of movies,Tv shows and people to discover.Exlpore now</span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show...."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button onKeyUp={searchQueryHandler}>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+
+    </div>
+  )
+}
+
+export default HomeBanner
