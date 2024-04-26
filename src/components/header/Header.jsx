c0e4a7d4bde0aa2f607ec23abdf6c605796c8a3f
adityap5 <aditya29pippal@gmail.com>
2024-04-27 01:49:45 +0530

    #

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,44 +5,41 @@ import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
 import './style.scss'
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
-    const [show, setShow] = useState("top");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-
-    useEffect(() => {
-      window.scrollTo(0,0)
-    }, [location])
-    
-  const controlNavbar =()=>{
-    if(window.scrollY >200){
-      if(window.scrollY > lastScrollY){
-        setShow("hide")
-      }else{
-        setShow("show");
-      }
-    }else{
-      setShow("show");
-    }
-    
-
-    setLastScrollY(window.scrollY)
-  }
   useEffect(() => {
-    window.addEventListener("scroll",controlNavbar)
-    return ()=>{
-    window.removeEventListener("scroll",controlNavbar)
+      window.scrollTo(0, 0);
+  }, [location]);
 
-    }
-  }, [lastScrollY])
-  
+  const controlNavbar = () => {
+      if (window.scrollY > 200) {
+          if (window.scrollY > lastScrollY && !mobileMenu) {
+              setShow("hide");
+          } else {
+              setShow("show");
+          }
+      } else {
+          setShow("top");
+      }
+      setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+      window.addEventListener("scroll", controlNavbar);
+      return () => {
+          window.removeEventListener("scroll", controlNavbar);
+      };
+  }, [lastScrollY]);
+
 
     const openSearch = ()=>{
       setMobileMenu(false)
